c0fdce7463b1a2ac79c7c18db495d33211e2fb85
Vladislav Kosenko <katko5@mail.ru>
2023-05-08 14:36:47 +0500

    New commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service;
+        // = new BmiService();
+
+        //*int price = 10_000;
+        //int miles = service.calculate(price); // должно получиться 500
+        //System.out.println(miles);*/
+
+
+      /*  int weight = 100;
+        double height = 1.7;
+        double h;
+        h = height * height;
+        int index;
+        int bmi = (int) (weight / h);
+
+        System.out.println("Your Body mass index:" + bmi);*/
+
+    }
+}
